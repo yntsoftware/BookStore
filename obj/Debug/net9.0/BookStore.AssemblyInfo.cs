776821d2081e355ef7fdb8991123dbaafc736437
Nuey Thepyasuwan <nuey@yntsoftware.com>
2024-12-15 18:57:50 -0800

    Initial build and run successful

--- obj/Debug/net9.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a29b251999c67b443db29da8bb72afd9c01f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
