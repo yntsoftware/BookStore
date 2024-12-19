e3786364830e8b2470cdb6196cb7bace038cd3cb
Nuey Thepyasuwan <nuey@yntsoftware.com>
2024-12-19 01:50:49 -0800

    Created Categories table in Sql Server using EF Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data;
+
+public class ApplicationDbContext : DbContext
+{
+  public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+  {
+
+  }
+  public DbSet<Category> Categories { get; set; }
+}
--- obj/Debug/net9.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776821d2081e355ef7fdb8991123dbaafc736437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed50b523f8e1d3fd4a37faadfc85c4acc5fc7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
